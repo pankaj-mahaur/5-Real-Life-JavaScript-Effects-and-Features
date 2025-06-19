bc10f29bd4d9613d42bdd51b6e4e46cd2b6d8770
pankaj-mahaur <pankajmahaur2003@gmail.com>
2025-06-20 02:54:25 +0530

    first one done

---
+++ script.js
@@ -0,0 +1,20 @@
+var istatus = document.querySelector("h4")
+
+var addFriend = document.querySelector("#add")
+var check = 0
+
+addFriend.addEventListener("click", function(){
+    if(check == 0){
+    istatus.innerHTML = "Friends"
+    istatus.style.color = "green"
+    addFriend.innerHTML = "Remove Friend"
+    check = 1
+}else{
+    istatus.innerHTML = "Stranger"
+    istatus.style.color = "red"
+    addFriend.innerHTML = "Add Friend"
+    check = 0
+}
+
+}
+)
