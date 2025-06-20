647805ba689469dd4a0bfca9cee445452d585afe
pankaj-mahaur <pankajmahaur2003@gmail.com>
2025-06-21 01:09:55 +0530

    insta like icon popup implement

--- script.js
+++ script.js
@@ -18,3 +18,22 @@ addFriend.addEventListener("click", function(){
 
 }
 )
+
+// 2nd Section
+
+var sec = document.querySelector("#container")
+var icon = document.querySelector("i")
+
+sec.addEventListener("dblclick", function(){
+    icon.style.transform = 'translate(-50%, -50%) scale(1)';
+    icon.style.opacity = 0.8
+
+    setTimeout(() => {
+    icon.style.opacity = 0
+}, 1000);
+    setTimeout(() => {
+    icon.style.transform = 'translate(-50%, -50%) scale(0)'
+}, 1500);
+})
+
+
